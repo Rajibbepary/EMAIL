0e3f4717d9d1efd9a6aa7fcbc24136dfdc8d7627
Rajibbepary <rajib635356@gmail.com>
2024-07-03 14:30:41 +0600

    my first email vaild profolio

---
+++ text.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+// $(document).ready(function (){
+//     let offset = { offset:"80%"}
+//         $("#sub_titel").waypoint(function (){
+  
+//           $("#sub_titel").addClass(
+//               "animate__animated animate__fadeInLeft");
+//         },
+//         offset );
+  
+//   });
